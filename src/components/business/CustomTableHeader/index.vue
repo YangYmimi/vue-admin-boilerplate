7e2cbbde3665c94774078fb4f6b6d8768b546d34
Superman <Ymimi1993@hotmail.com>
2021-03-01 23:02:25 +0800

    init custom-table-header

---
+++ src/components/business/CustomTableHeader/index.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="cpt-business-customtableheader-wrapper">
+    <span class="header-title">{{ name }}</span>
+    <span class="header-sort">
+      <i class="el-icon-caret-top"></i>
+      <i class="el-icon-caret-bottom"></i>
+    </span>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "BusinessCustomTableHeader",
+  props: {
+    name: {
+      type: String,
+      required: true
+    },
+    options: {
+      type: Object,
+      default: () => {}
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped></style>
